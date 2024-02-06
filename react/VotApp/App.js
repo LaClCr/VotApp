6f4af35f49ba99752e26a5d3f481dfca353a0905
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2024-02-06 15:34:59 +0100

    Instalando dependencias

--- react/VotApp/App.js
+++ react/VotApp/App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import { Text, BottomNavigation } from 'react-native-paper';
+
 
 export default function App() {
   return (
