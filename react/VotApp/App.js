cc33c0da040f4ccc9a72556e74c2c1fad1148152
DavidReinon <davidreinongarcia@gmail.com>
2024-02-08 20:01:06 +0100

    dependencias actualizadas y pequeños cambios

--- react/VotApp/App.js
+++ react/VotApp/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
@@ -14,73 +14,86 @@ import Settings from "./src/screens/settings/Settings";
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Componente intermedio que decide qué mostrar en el tab "Create"
-  function CreateStack() {
-    return (
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="CodeAccess" component={CodeAccess} />
-        <Stack.Screen name="ProjectCreation" component={ProjectCreation} />
-      </Stack.Navigator>
-    );
-  }
-
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Home"
-          activeColor="#3B0809"
-          inactiveColor="#f0edf6"
-          barStyle={{ backgroundColor: "#C02830" }}
-        >
-          <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{
-              tabBarLabel: "Home",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="CameraQR"
-            component={CameraQR}
-            options={{
-              tabBarLabel: "Cámara",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="camera" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Create"
-            component={CreateStack} // Cambiado de CreateScreen a CreateStack
-            options={{
-              tabBarLabel: "Create",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="plus-box"
-                  color={color}
-                  size={26}
+    // Componente intermedio que decide qué mostrar en el tab "Create"
+    function CreateStack() {
+        return (
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="CodeAccess" component={CodeAccess} />
+                <Stack.Screen
+                    name="ProjectCreation"
+                    component={ProjectCreation}
                 />
-              ),
-            }}
-          />
-           <Tab.Screen
-            name="Settings"
-            component={Settings}
-            options={{
-              tabBarLabel: "Settings",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name='cog' color={color} size={26} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+            </Stack.Navigator>
+        );
+    }
+
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Tab.Navigator
+                    initialRouteName="Home"
+                    activeColor="#3B0809"
+                    inactiveColor="#f0edf6"
+                    barStyle={{ backgroundColor: "#C02830" }}
+                >
+                    <Tab.Screen
+                        name="Home"
+                        component={Home}
+                        options={{
+                            tabBarLabel: "Home",
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name="home"
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="CameraQR"
+                        component={CameraQR}
+                        options={{
+                            tabBarLabel: "Cámara",
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name="camera"
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Create"
+                        component={CreateStack} // Cambiado de CreateScreen a CreateStack
+                        options={{
+                            tabBarLabel: "Create",
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name="plus-box"
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Settings"
+                        component={Settings}
+                        options={{
+                            tabBarLabel: "Settings",
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name="cog"
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 }
