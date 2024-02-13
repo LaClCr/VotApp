ffbf3463606346381114741442aad2d9a1cedc69
DavidReinon <davidreinongarcia@gmail.com>
2024-02-13 16:37:52 +0100

    Header Florida cambiado para android

--- react/VotApp/src/components/FloridaHeader.js
+++ react/VotApp/src/components/FloridaHeader.js
@@ -2,7 +2,7 @@ import { Image, StyleSheet } from "react-native";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 import React from "react";
-import FloridaStandardLogo from "../assets/floridaLogoTransparente.png";
+import FloridaStandardLogo from "../assets/floridaLogoTransparenteV2.png";
 import FloridaDarkLogo from "../assets/floridaLogoNegro.png";
 
 const FloridaHeader = () => {
@@ -11,7 +11,7 @@ const FloridaHeader = () => {
     return (
         <Image
             source={isDarkMode ? FloridaDarkLogo : FloridaStandardLogo}
-            style={isDarkMode ? styles.darkLogo : styles.standardLogo}
+            style={styles.standardLogo}
         />
     );
 };
@@ -20,11 +20,6 @@ export default FloridaHeader;
 
 const styles = StyleSheet.create({
     standardLogo: {
-        resizeMode: "center",
-        height: 50,
-        marginBottom: 20,
-    },
-    darkLogo: {
         resizeMode: "contain",
         height: 50,
         marginBottom: 20,
