41d5893515266756873ae7957ee5ef4e951f9b47
Pitusa <jupexx@floridauniversitaria.es>
2024-02-13 17:46:41 +0100

    Prueba de cámara comentado

--- react/VotApp/src/screens/scannerQR/CameraQR.js
+++ react/VotApp/src/screens/scannerQR/CameraQR.js
@@ -59,3 +59,89 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
 });
+
+
+/*
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Camera } from 'expo-camera';
+import ScreensContext from '../projectView/projectViewScreensContext';
+
+const QRReader = () => {
+
+    const { setProjectName } = useContext(ScreensContext);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+
+        console.log(data);
+
+        if (data) {
+            setProjectName(data);
+            navigation.navigate("ProjectDetails");
+        }
+    };
+
+    if (hasPermission === null) {
+        return <Text>Solicitando permiso para acceder a la cámara...</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No se ha concedido acceso a la cámara</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Camera
+                style={styles.camera}
+                type={Camera.Constants.Type.back}
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            >
+                <View style={styles.scanBar} />
+            </Camera>
+            {scanned && (
+                <View style={styles.sectionButton}>
+                    <Button
+                        onPress={() => setScanned(false)}
+                        mode="contained"
+                        buttonColor="#C02830">Escanear de nuevo</Button>
+                </View> 
+            )}
+        </View>
+    );
+};
+
+export default QRReader;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    camera: {
+        flex: 1,
+    },
+    scanBar: {
+        width: '100%',
+        height: 2,
+        backgroundColor: 'red',
+    },
+    sectionButton: {
+        padding: 20,
+        elevation: 5,
+        justifyContent: 'center',
+        backgroundColor: '#C02830',
+    },
+});
+*/
